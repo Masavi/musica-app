6baa0ced2ebec64289081951694ad9aed13e90c7
maui <maui@devf.mx>
2019-07-09 21:59:05 -0500

    agregar proyecto base

---
+++ src/Components/HolaMundo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HolaMundo extends Component {
+    render() { 
+        return ( 
+            <h1>Hola desde el componente Hola Mundo</h1>
+         );
+    }
+}
+ 
+export default HolaMundo;
